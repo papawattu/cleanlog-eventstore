e2a8b8d4d714e287615eb5f8e3ba6b8ea4870408
Jamie Nuttall <jamie@wattu.com>
2024-11-12 16:26:02 +0000

    refactored

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/papawattu/cleanlog-eventstore
 go 1.23.1
 
 require (
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/papawattu/cleanlog-tasks v0.0.1 // indirect
 	github.com/confluentinc/confluent-kafka-go v1.9.2
 
 )
---
+++ repository/repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import "context"
+
+type Repository[T any, S comparable] interface {
+	Save(ctx context.Context, entity T) error
+	Get(ctx context.Context, ID S) (T, error)
+	GetAll(ctx context.Context) ([]T, error)
+	Delete(ctx context.Context, e T) error
+	Exists(ctx context.Context, ID S) (bool, error)
+	GetId(ctx context.Context, e T) (S, error)
+}
